23d462a51c1173b30126997581fe4205b1e7af66
Kunal Patel <kunal1990patel@gmail.com>
2020-06-20 18:46:52 +0530

    Commit By Kunal

--- routes/login.js
+++ routes/login.js
@@ -3,6 +3,37 @@ const logger = require('../logger/logger');
 const jwt = require('jsonwebtoken');
 const app = express();
 var connection = require('../config/db');
+
+/* Mailer Code Start */
+const nodemailer = require('nodemailer');
+
+// For Other Auth
+// let transport = nodemailer.createTransport({
+//     host: 'smtp.mailtrap.io',
+//     port: 2525,
+//     auth: {
+//        user: 'put_your_username_here',
+//        pass: 'put_your_password_here'
+//     }
+// });
+
+// For Gmail Auth
+// var transporter = nodemailer.createTransport({
+//   service: 'gmail',
+//   port:465,
+//   auth: {
+//     user: 'kunal1990patel@gmail.com',
+//     pass: '#######'
+//   }
+// });
+
+// For localhost Auth
+var transporter = nodemailer.createTransport({
+  host: '125.0.0.1',
+  port:25
+});
+
+/* Mailer Code End */
   
 let refreshTokens = [];
 
@@ -36,8 +67,7 @@ app.post('/login',(req,res) =>{
   //Authenticate user
   const username = req.body.Email;
   const password = req.body.Password;
-  var params  = req.body;
-  connection.query('SELECT * FROM `tbl_registration` WHERE username="'+username+'" and password="'+password+'"', function (error, results, fields) {
+  connection.query('SELECT id,firstname,lastname,email,phone,userguid FROM `tbl_registration` WHERE username="'+username+'" and password="'+password+'"', function (error, results, fields) {
     if (error) throw error;
     if(results.length)
     {
@@ -65,5 +95,43 @@ function generateAccessToken(user){
 function generateRefreshToken(user){
   return jwt.sign(user,process.env.REFRESH_TOKEN_SECRET)
 }
+
+app.post('/PasswordRecovery',(req,res) =>{
+  //Authenticate user
+  const username = req.body.Email;
+  connection.query('SELECT email,userguid FROM `tbl_registration` WHERE username="'+username+'"', function (error, results, fields) {
+    if (error) throw error;
+    if(results.length)
+    {
+      var mailOptions = {
+        from: 'iampatelprince@gmail.com',
+        to: results.email,
+        subject: 'Password Recovery Mail',
+        text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
+        'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
+        'http://localhost:4200/#/passwordreset/' + results.userguid + '\n\n' +
+        'If you did not request this, please ignore this email and your password will remain unchanged.\n'
+      };
+      
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+          res.json({ Message:"success"});
+        }
+      });
+      
+    }
+    else
+    {
+      res.json({ Message:"Username is wrong. Please Try Again.",results});
+    }
+    
+  });
+
+  
+  // res.json({ accessToken : accessToken, refreshToken : refreshToken});
+});
 
 module.exports = app;
