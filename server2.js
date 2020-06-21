e61a469457c6df220a2957c980dad3da087070de
Sujan Valand <sujanvaland@gmail.com>
2020-06-20 21:41:45 -0700

    static url serve

---
+++ server2.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+
+//setting middleware
+app.use(express.static('public'));
+app.use(express.static('uploads'));
+app.use(express.static('categoryicon'));
+
+var server = app.listen(5000);
