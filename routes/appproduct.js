2af23b09def13ec589e8b6a8031ad9bc34fcfcc5
Kunal Patel <kunal1990patel@gmail.com>
2020-06-28 17:20:26 +0530

    Commit By Kunal New arrival API

--- routes/appproduct.js
+++ routes/appproduct.js
@@ -31,5 +31,13 @@ app.post('/productdetail', function (req, res) {
       res.json({ Message:"success",results});
     });
 });
+
+//rest api to get new arival product
+app.get('/newarrivalproduct', function (req, res) {
+  connection.query('select * from tbl_product where isactive=1 and isnewarrival=1 order by sortorder asc limit 2', function (error, results, fields) {
+      if (error) throw error;
+      res.json({ Message:"success",results});
+    });
+});
   
   module.exports = app;
