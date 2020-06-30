98a3ecaaf8295af9f9f663f32beb1631c6c7eac5
Kunal Patel <kunal1990patel@gmail.com>
2020-06-30 05:33:58 +0530

    Commited By Kunal

--- routes/appproduct.js
+++ routes/appproduct.js
@@ -34,7 +34,7 @@ app.post('/productdetail', function (req, res) {
 
 //rest api to get new arival product
 app.get('/newarrivalproduct', function (req, res) {
-  connection.query('select * from tbl_product where isactive=1 and isnewarrival=1 order by sortorder asc limit 2', function (error, results, fields) {
+  connection.query('select * from tbl_product where isactive=1 and isnewarrival=1 ORDER BY RAND() asc limit 2', function (error, results, fields) {
       if (error) throw error;
       res.json({ Message:"success",results});
     });
