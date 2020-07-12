340f039f1d228a6e4ace571f33668d4db7c47bad
Kunal Patel <kunal1990patel@gmail.com>
2020-07-12 19:07:01 +0530

    Commit By Place Order API

--- routes/cart.js
+++ routes/cart.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 var connection = require('../config/db');
+const { v1: uuidv1 } = require('uuid');
 
   
   //rest api to add cart data
@@ -94,6 +95,92 @@ var connection = require('../config/db');
          }
          res.json({ Message:"success",totalcartcount:totalcartcount});
        });
+  });
+
+  //rest api to add cart data
+  app.post('/placeorder', function (req, res) {
+    var params  = req.body;
+    var sql = "SELECT tbl_cart.id,sum(tbl_product.price*tbl_cart.qty) as subtotal FROM tbl_cart JOIN tbl_product ON tbl_cart.pid = tbl_product.id where qty > 0 and userguid='"+req.headers.customerguid+"' and tbl_product.isactive=1 and tbl_product.instock=1";
+    connection.query(sql, function (error, results, fields) {
+      if (error) throw error;
+      if(results.length == 0)
+      {
+        res.json({ Message:"error",error_message:"There are no items into cart. Please Try Again."});
+      }
+      else
+      {
+        connection.query('select id,addresstype,bfirstname,blastname,bphone,door_no_build_no_street,locality,landmark,bcity,bzipcode,isdefault,latitude,longitude from tbl_manage_address where userguid="'+req.headers.customerguid+'" and id="'+req.body.addressid+'"', function (error, addressresults, fields) {
+          if (error) throw error;
+          if(addressresults.length > 0)
+          {
+            var orderstatus = 0;
+            if (req.body.paymenttype ==1)
+            {
+              orderstatus = 1;
+            }
+            var subtotal=results[0].subtotal;
+            var deliverycharge=0;
+            var ordertotal=(subtotal - 0) + (deliverycharge - 0);
+            var orderguid=uuidv1();
+
+            var newparams={
+              "addresstype":addressresults[0].addresstype,
+              "firstname":addressresults[0].bfirstname,
+              "lastname":addressresults[0].blastname,
+              "phone":addressresults[0].bphone,
+              "email":'',
+              "door_no_build_no_street":addressresults[0].door_no_build_no_street,
+              "locality":addressresults[0].locality,
+              "landmark":addressresults[0].landmark,
+              "city":addressresults[0].bcity,
+              "zipcode":addressresults[0].bzipcode,
+              "latitude":addressresults[0].latitude,
+              "longitude":addressresults[0].longitude,
+              "orderguid":orderguid,
+              "ordertotal":ordertotal,
+              "userguid":req.headers.customerguid,
+              "subtotal":subtotal,
+              "paymenttype":req.body.paymenttype,
+              "grandtotal":ordertotal,
+              "deliverycharge":deliverycharge,
+              "orderstatus":orderstatus,
+              "orderdate": new Date()
+            }
+            
+            // For Insert Order
+            connection.query('INSERT INTO `tbl_order` SET ?', newparams, function (error, Insertresults, fields) {
+              if (error) throw error;
+              // For Insert Order Items
+              if(Insertresults.insertId > 0)
+              {
+                var orderitemsql ="insert into tbl_orderitem(pid,productname,pieces,net_weight,productimage,orderguid,qty,pprice,unitprice,mainprice) select pid,(select productname from tbl_product where id=tbl_cart.pid) as productname,(select pieces from tbl_product where id=tbl_cart.pid) as pieces,(select net_weight from tbl_product where id=tbl_cart.pid) as net_weight,(select productimage from tbl_product where id=tbl_cart.pid) as productimage,'"+orderguid+"',qty,((select price from tbl_product where id=tbl_cart.pid)*qty) as pprice,(select price from tbl_product where id=tbl_cart.pid) as unitprice,(select price from tbl_product where id=tbl_cart.pid) as mainprice from tbl_cart where userguid='"+req.headers.customerguid+"'";
+                connection.query(orderitemsql, function (error, InsertItemresults, fields) {
+                    if (error) throw error;
+                    // For Delete Cart
+                    connection.query('DELETE FROM tbl_cart WHERE userguid="'+req.headers.customerguid+'"', function (error, results, fields) {
+                      if (error) throw error;
+                      res.json({ Message:"success",orderguid:orderguid});
+                    });
+                });
+              }
+              else
+              {
+                connection.query('DELETE FROM tbl_order WHERE id="'+Insertresults.insertId+'"', function (error, results, fields) {
+                  if (error) throw error;
+                  res.json({ Message:"error",error_message:"Something went wrong. Please try again."});
+                });
+              }
+              
+            });
+          }
+          else
+          {
+            res.json({ Message:"error",error_message:"Please select atleast one address."});
+          }
+        });
+        
+      }
+    });
   });
 
   
