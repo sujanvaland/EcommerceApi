e7ccb3d5e0011e6b00d41fc03432b4c12ded20b0
Kunal Patel <kunal1990patel@gmail.com>
2020-07-18 01:51:31 +0530

    Commit By Kunal

--- routes/order.js
+++ routes/order.js
@@ -5,7 +5,7 @@ var connection = require('../config/db');
   //rest api to get all orders
   app.get('/order', function (req, res) {
     var callbackCounter = 0;
-    connection.query('select * from tbl_order', function (error, results, fields) {
+    connection.query('select *,(select CONCAT(firstname," ",lastname) from tbl_registration where userguid=tbl_order.staff_id) as staffname from tbl_order', function (error, results, fields) {
         if (error) throw error;
         let orders = [];
         results.forEach(element => { 
@@ -28,7 +28,7 @@ var connection = require('../config/db');
   
   app.get('/GetAllOrders', function (req, res) {
     var callbackCounter = 0;
-    connection.query('select * from tbl_order', function (error, results, fields) {
+    connection.query('select *,(select CONCAT(firstname," ",lastname) from tbl_registration where userguid=tbl_order.staff_id) as staffname from tbl_order', function (error, results, fields) {
         if (error) throw error;
         let orders = [];
         results.forEach(element => { 
@@ -52,31 +52,38 @@ var connection = require('../config/db');
   //rest api to get filter order
   app.get('/GetAllOrders/:name', function (req, res) {
      var callbackCounter = 0;
-     connection.query('select * from tbl_order where (firstname like "%'+req.params.name+'%" or lastname like "%'+req.params.name+'%")', function (error, results, fields) {
+     connection.query('select *,(select CONCAT(firstname," ",lastname) from tbl_registration where userguid=tbl_order.staff_id) as staffname from tbl_order where (firstname like "%'+req.params.name+'%" or lastname like "%'+req.params.name+'%")', function (error, results, fields) {
          if (error) throw error;
-         let orders = [];
-         results.forEach(element => { 
-           connection.query('select * from tbl_orderitem where orderguid="'+element.orderguid+'"', function (error, itemresults, fields) {
-             if (error) throw error;
-             element.orderitems = [];
-             itemresults.forEach(newelement => {
-               element.orderitems.push(newelement);
-             });
-             orders.push(element);
-             callbackCounter++;
-             if(results.length == callbackCounter)
-             {
-               res.send(orders);
-             }
-           });
-         });
+         if(results.length > 0)
+         {
+          let orders = [];
+          results.forEach(element => { 
+            connection.query('select * from tbl_orderitem where orderguid="'+element.orderguid+'"', function (error, itemresults, fields) {
+              if (error) throw error;
+              element.orderitems = [];
+              itemresults.forEach(newelement => {
+                element.orderitems.push(newelement);
+              });
+              orders.push(element);
+              callbackCounter++;
+              if(results.length == callbackCounter)
+              {
+                res.send(orders);
+              }
+            });
+          });
+         }
+         else
+         {
+          res.send(results);
+         }
       });
   });
 
   //rest api to get a login order data
   app.post('/GetOrderInfo', function (req, res) {
     var callbackCounter = 0;
-    connection.query('select * from tbl_order where orderguid="'+req.body.orderguid+'"', function (error, results, fields) {
+    connection.query('select *,(select CONCAT(firstname," ",lastname) from tbl_registration where userguid=tbl_order.staff_id) as staffname from tbl_order where orderguid="'+req.body.orderguid+'"', function (error, results, fields) {
         if (error) throw error;
         let orders = [];
         results.forEach(element => { 
@@ -100,7 +107,7 @@ var connection = require('../config/db');
   //rest api to get a single tbl_order data
   app.get('/order/:id', function (req, res) {
      var callbackCounter = 0;
-     connection.query('select * from tbl_order where id="'+req.params.id+'"', function (error, results, fields) {
+     connection.query('select *,(select CONCAT(firstname," ",lastname) from tbl_registration where userguid=tbl_order.staff_id) as staffname from tbl_order where id="'+req.params.id+'"', function (error, results, fields) {
          if (error) throw error;
          let orders = [];
          results.forEach(element => { 
@@ -135,6 +142,94 @@ var connection = require('../config/db');
      if (error) throw error;
        res.send('Record has been deleted!');
      });
+  });
+
+  //rest api to update order status into mysql database
+  app.post('/ChangeStatus', function (req, res) {
+      if(req.body.orderstatus==6)
+      {
+        connection.query('select staff_id from tbl_order where orderguid="'+req.body.orderguid+'"', function (error, orderresults, fields) {
+          if (error) throw error;
+          if(orderresults.length > 0)
+          {
+            connection.query('select userguid,assignorder from tbl_registration where isactive=1 and role_id=2 and userguid="'+orderresults[0].staff_id+'"', function (error, results, fields) {
+              if (error) throw error;
+              if(results.length > 0)
+              {
+                var oldassignorder=results[0].assignorder;
+                var newone=1;
+                var assignorder=0;
+                if(oldassignorder > 0)
+                {
+                  assignorder=(oldassignorder - 0) - (newone - 0);
+                }
+                
+                connection.query('UPDATE `tbl_registration` SET `assignorder`=? where `userguid`=?', [assignorder, orderresults[0].staff_id], function (error, results, fields) {
+                  if (error) throw error;
+                  connection.query('UPDATE `tbl_order` SET `orderstatus`=?,`staff_id`=? where `orderguid`=?', [req.body.orderstatus,null, req.body.orderguid], function (error, results, fields) {
+                    if (error) throw error;
+                    res.send({Message:"success"});
+                  });
+                });
+              }
+              else
+              {
+                connection.query('UPDATE `tbl_order` SET `orderstatus`=? where `orderguid`=?', [req.body.orderstatus, req.body.orderguid], function (error, results, fields) {
+                  if (error) throw error;
+                  res.send({Message:"success"});
+                });
+              }
+            });
+          }
+          else
+          {
+            connection.query('UPDATE `tbl_order` SET `orderstatus`=? where `orderguid`=?', [req.body.orderstatus, req.body.orderguid], function (error, results, fields) {
+              if (error) throw error;
+              res.send({Message:"success"});
+            });
+          }
+        });
+      }
+      else
+      {
+        connection.query('UPDATE `tbl_order` SET `orderstatus`=? where `orderguid`=?', [req.body.orderstatus, req.body.orderguid], function (error, results, fields) {
+          if (error) throw error;
+          res.send({Message:"success"});
+        });
+      }
+  });
+
+  //rest api to get all active staff into mysql database
+  app.get('/GetAllActiveStaff', function (req, res) {
+    connection.query('select firstname,lastname,userguid,assignorder from tbl_registration where isactive=1 and role_id=2 and onride=0 and assignorder < 2', function (error, results, fields) {
+        if (error) throw error;
+        res.send(results);
+     });
+  });
+
+  //rest api to assign staff to order into mysql database
+  app.post('/AssignStaff', function (req, res) {
+    connection.query('select userguid,assignorder from tbl_registration where isactive=1 and role_id=2 and onride=0 and assignorder < 2 and userguid="'+req.body.staff_id+'"', function (error, results, fields) {
+        if (error) throw error;
+        if(results.length > 0)
+        {
+          connection.query('UPDATE `tbl_order` SET `staff_id`=? where `orderguid`=?', [req.body.staff_id, req.body.orderguid], function (error, orderresults, fields) {
+            if (error) throw error;
+              var oldassignorder=results[0].assignorder;
+              var newone=1;
+              var assignorder=(oldassignorder - 0) + (newone - 0);
+              connection.query('UPDATE `tbl_registration` SET `assignorder`=? where `userguid`=?', [assignorder, req.body.staff_id], function (error, results, fields) {
+                if (error) throw error;
+                  res.send({Message:"success",results});
+                });
+            });
+        }
+        else
+        {
+          res.send({Message:"error"});
+        }
+    });
+    
   });
   
   module.exports = app;
